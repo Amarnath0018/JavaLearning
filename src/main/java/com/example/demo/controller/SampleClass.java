96de645988dca6048987576062dac825687a13a5
Amarnath0018 <104964783+Amarnath0018@users.noreply.github.com>
2025-12-29 23:03:57 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/SampleClass.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+public class SampleClass {
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        System.out.println("helloWorld method starts at "+ Instant.now());
+        return "Hello World";
+    }
+}
